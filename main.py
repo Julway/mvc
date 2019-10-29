60dc984e8aaeaf24f1ed3991099911c4cfa6c2db
Julway <anna_gorny@hotmail.com>
2019-10-29 15:38:33 +0100

    make changes

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import datetime
 
 app = Flask(__name__)
 
+
 @app.route("/") #pfad
 def index():
-    return render_template("index.html")
+    welcometext = "Herzlich Willkommen"
+    currentyear = datetime.datetime.now().year
+    vemap=["Martin", "Hani", "Anna", "Markus"]
+    return render_template("index.html", welcometext=welcometext, currentyear=currentyear, vemap=vemap)
+
 
 @app.route("/about-me")
 def about():
     return render_template("about.html")
+
 
 @app.route("/help-me")
 def help():
     return render_template("help.html")
 
 
-if __name__=='__main__':
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+    return render_template("success.html")
+
+
+if __name__ == '__main__':
     app.run()
